3f6d5d0b71531f7dfa1ec970a1ae4128545b2961
JulioDaltro-Hub <59486522+Jota-hub@users.noreply.github.com>
2022-03-10 21:49:00 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Athena Grega',
+                partido: 'GREGO',
+                fotos: [
+                    {url:'athena.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '15151',
+                nome: 'Hades Romano',
+                partido: 'ROMA',
+                fotos: [
+                    {url:'romHades.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Hades Grego',
+                partido: 'GREGO',
+                vice: 'Cic',
+                fotos: [
+                    {url: 'hades.png', legenda: 'Prefeito'},
+                    {url: 'athena.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '84',
+                nome: 'Athena Romana',
+                partido: 'Roma',
+                vice: 'Hades',
+                fotos: [
+                    {url: 'romAthena.jpg', legenda: 'Prefeito'},
+                    {url: 'romHades.png', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
